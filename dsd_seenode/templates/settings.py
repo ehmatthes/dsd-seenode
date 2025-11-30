2df338c2028ca3bdebd85b01ef8cd4e888b655e8
Eric Matthes <ehmatthes@gmail.com>
2025-11-30 09:34:57 -0500

    Modify settings, no conditional block yet.

--- dsd_seenode/templates/settings.py
+++ dsd_seenode/templates/settings.py
@@ -1,3 +1,23 @@
 {{current_settings}}
 
 # seenode settings.
+import os
+import dj_database_url
+
+# Security settings
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+
+# Database configuration
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
+
+# Static files with WhiteNoise
+i = MIDDLEWARE.index("django.middleware.security.SecurityMiddleware")
+MIDDLEWARE.insert(i + 1, "whitenoise.middleware.WhiteNoiseMiddleware")
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
